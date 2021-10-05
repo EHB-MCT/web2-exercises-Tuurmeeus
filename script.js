9c8d45eeb1bfcb09d384642bbb6e4641648a1ba6
Tuurmeeus <50370038+Tuurmeeus@users.noreply.github.com>
2021-10-05 11:09:18 +0200

    Ophalen API

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 'use strict';
 
-fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-.then(response => response.json())
-.then(data => data.forEach(function(e){
-    console.log(e.name);
-}));
+let list = [];
+let pokemon = [];
 
+function getData(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(response => {
+            response.json().then(data => {
+                list = data.results;
+                //loop over list to get each pokemon
+                for(let element of list){
+                    fetch(element.url).then(response => {
+                        return response.json();
+                    }).then(data => {
+                        pokemon.push(data);
+                    })
+                }
+
+            })
+    });
+}
+
+window.onload = function(){
+    setTimeout(buildList, 5000);
+
+    function buildList(){
+        //opmaak div
+    }
+
+}
+
+/**function getNewData(){
+    let resp = fetch();
+    return await resp.json();
+}*/
+
+getData();
